8626e1d040a8e389a67129403d2144d0b50341df
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-24 23:39:21 -0300

    Updates

---
+++ precache-manifest.53b471f569538c8bfc88d64ca8088ecd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bdf1d02d0083c98d7db1d9b2e53dac",
+    "url": "/TrybeWallet-deploy/index.html"
+  },
+  {
+    "revision": "4794f4ec604c2298509f",
+    "url": "/TrybeWallet-deploy/static/css/main.fee6e5b3.chunk.css"
+  },
+  {
+    "revision": "ad17e9ac144056ce8e10",
+    "url": "/TrybeWallet-deploy/static/js/2.e21d0c8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TrybeWallet-deploy/static/js/2.e21d0c8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4794f4ec604c2298509f",
+    "url": "/TrybeWallet-deploy/static/js/main.6d79c94c.chunk.js"
+  },
+  {
+    "revision": "ce0657e968e929ec8725",
+    "url": "/TrybeWallet-deploy/static/js/runtime-main.0be1db0e.js"
+  }
+]);
